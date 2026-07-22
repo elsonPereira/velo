65c6812ea4d9b8a33f3d0a52835d328f34c13a31
Elson Pereira <elson.pereira@outlook.com>
2026-07-21 22:38:59 -0300

    estágio de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,10 +37,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://velo-cf3fl14v0-brito-d-elson.vercel.app/',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
 
     // Tempo máximo para ações interativas como click(), fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
